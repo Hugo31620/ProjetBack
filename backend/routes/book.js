aeed467998c22d628de7e4aaee9998ca43573fe3
Hugo31620 <mintegui.hugo@gmail.com>
2024-06-25 10:54:15 +0200

    Ajout code Routes avec cours OP et adaptation avec les besoins : user et book (Tous ,un ,crée, 2 rating, supprimé, update)

---
+++ backend/routes/book.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const router = express.Router();
+const auth = require('../middleware/auth');
+const multer = require("../middleware/multer-config")
+const booksCtrl = require('../controllers/book');
+
+router.get("/bestrating", booksCtrl.getBestRatedBooks);
+
+router.get('/',booksCtrl.getAllBooks);
+
+router.post('/',auth, multer, booksCtrl.createBook);
+
+router.get("/:id", booksCtrl.getOneBook);
+router.post("/:id/rating", auth, booksCtrl.setBookRating);
+router.put('/:id', auth, multer, booksCtrl.updateBook);
+router.delete('/:id', auth, booksCtrl.deleteBook);
+
+module.exports = router;
