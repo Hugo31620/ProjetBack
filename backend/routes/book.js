6c245d73a85c4c762363bc8a9b6f78e9a5357313
Hugo31620 <mintegui.hugo@gmail.com>
2024-07-04 18:28:10 +0200

    organisation des routes

--- backend/routes/book.js
+++ backend/routes/book.js
@@ -4,15 +4,15 @@ const auth = require('../middleware/auth');
 const multer = require("../middleware/multer-config")
 const booksCtrl = require('../controllers/book');
 
-router.get("/bestrating", booksCtrl.getBestRatedBooks);
-
+router.get('/bestrating', booksCtrl.bestrating);
 router.get('/',booksCtrl.getAllBooks);
+router.get("/:id", booksCtrl.getOneBook);
 
 router.post('/',auth, multer, booksCtrl.createBook);
+router.post("/:id/rating", auth, booksCtrl.createRating);
 
-router.get("/:id", booksCtrl.getOneBook);
-router.post("/:id/rating", auth, booksCtrl.setBookRating);
 router.put('/:id', auth, multer, booksCtrl.updateBook);
+
 router.delete('/:id', auth, booksCtrl.deleteBook);
 
 module.exports = router;
