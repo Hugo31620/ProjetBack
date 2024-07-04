759f1664ce542e1deb5035840cf0049b7854df1d
Hugo31620 <mintegui.hugo@gmail.com>
2024-07-04 18:25:30 +0200

    mise en place des routes vers livres , auth et images statique

--- backend/app.js
+++ backend/app.js
@@ -2,19 +2,15 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const mongoose = require('mongoose');
+const bodyParser = require("body-parser");
+const bookRoutes = require("./routes/book");
+const userRoutes = require("./routes/user");
+const path = require("path");
 
-mongoose.connect('mongodb+srv://minteguihugo:MongoDB@cluster0.flzgo1k.mongodb.net/',
-    { useNewUrlParser: true,
-      useUnifiedTopology: true })
+mongoose.connect('mongodb+srv://minteguihugo:MongoDB@cluster0.flzgo1k.mongodb.net/', {
+  })
     .then(() => console.log('Connexion à MongoDB réussie !'))
     .catch(() => console.log('Connexion à MongoDB échouée !'));
-
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-  next();
-});
 
 app.use(cors({
     origin: "*",
@@ -24,23 +20,10 @@ app.use(cors({
     credentials: true
 }));
 
-app.use((req, res, next) => {
-    console.log('Requête reçue !');
-    next();
-});
-  
-app.use((req, res, next) => {
-    res.status(201);
-    next();
-});
-  
-app.use((req, res, next) => {
-    res.json({ message: 'Votre requête a bien été reçue !' });
-    next();
-});
-  
-app.use((req, res, next) => {
-    console.log('Réponse envoyée avec succès !');
-});
+app.use(bodyParser.json());
+app.use("/api/books", bookRoutes);
+app.use("/api/auth", userRoutes);
+app.use("/images", express.static(path.join(__dirname, "images")));
+
 
 module.exports = app;
